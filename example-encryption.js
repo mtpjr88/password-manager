866c63b7f0097124ec4bb75eaea5c4d45f3c921e
Michael Pimentel <mtpjr88@gmail.com>
2017-03-09 19:59:01 -0800

    some new encryptin for passwords

--- example-encryption.js
+++ example-encryption.js
@@ -1,43 +1,31 @@
 var crypto = require('crypto-js');
 
-var secretMessage = {
+var secretMessage = {  // object must convert to string 
 	name: 'Michael',
 	secretName: '007'
 };
-var secretKey = '123abc';
 
-// Encrypt
-/*
-var encryptedMessage = crypto.AES.encrypt(secretMessage, secretKey);
+var secretMessageObj = JSON.stringify(secretMessage); // now a string
+
+var secretKey = '1234';
+
+// Encrypt | to Hide
+
+var encryptedMessage = crypto.AES.encrypt(secretMessageObj, secretKey); // only takes a string
 console.log('Encrypted Message: ' + encryptedMessage);
+console.log("It is now a: " + typeof encryptedMessage);
 
 
-// Decrypt Message
+// Decrypt Message | to reveal
 
-var bytes = crypto.AES.decrypt(encryptedMessage, secretKey);
 
+
+var bytes = crypto.AES.decrypt(encryptedMessage, secretKey); 
 var decryptedMessage = bytes.toString(crypto.enc.Utf8);
-
 console.log('Decrypted Message: ' + decryptedMessage);
+console.log("It is now a: " + typeof decryptedMessage);
 
-*/
-
-
-	// converting my object to string
-var jSonSecretMessage = JSON.stringify(secretMessage); 
-
-	// encryting the string and logging it to the console
-var encryptMessage = crypto.AES.encrypt(jSonSecretMessage, secretKey);
-console.log('Secret Message object to string is: ' + encryptMessage);
-
-console.log('Now Decrypting...');
-
-	// Decrypting it back to an object
-var bytes = crypto.AES.decrypt(encryptMessage, secretKey);
-
-var decryptMess = bytes.toString(crypto.enc.Utf8);
-
-var decryptMessageObj = JSON.parse(decryptMess);
-console.log('From string back to Object: ' + decryptMessageObj);
-
-console.log(secretMessage.secretName);
+var backToObj = JSON.parse(decryptedMessage); // converting back to an object
+console.log('Now converting back to object... ');
+console.log("It is now an: " + " ' " + typeof backToObj + " ' " + " Once again!");
+console.log(secretMessage.name);
