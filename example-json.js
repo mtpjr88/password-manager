ec9f0c532ec75874ff1bbe5a8a8f3e0b013e2f63
Michael Pimentel <mtpjr88@gmail.com>
2017-03-09 18:05:56 -0800

    string and object parse practice

--- example-json.js
+++ example-json.js
@@ -10,3 +10,18 @@ console.log(typeof personJSON);
 var personObj = JSON.parse(personJSON);
 console.log(personObj.name);
 console.log(typeof personObj);
+
+console.log('Challenge Area');
+
+var animal = '{"name": "Maggie"}';
+// convert to js object
+var animalObj = JSON.parse(animal);
+
+// add age property
+animalObj.age = 7;
+
+// convert back to JSON
+var animalJSON = JSON.stringify(animalObj);
+console.log(animalJSON);
+
+//
